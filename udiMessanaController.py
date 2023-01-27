14ebc1e9657245c9f18a21b8e684515093c62987
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2023-01-27 10:38:14 -0800

    Update udiMessanaController.py

--- udiMessanaController.py
+++ udiMessanaController.py
@@ -438,7 +438,7 @@ if __name__ == "__main__":
     try:
         logging.info('Starting Messana Controller')
         polyglot = udi_interface.Interface([])
-        polyglot.start('0.0.56')
+        polyglot.start('0.0.57')
         MessanaController(polyglot, 'system', 'system', 'Messana Radiant System')
 
         # Just sit and wait for events
